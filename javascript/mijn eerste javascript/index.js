461a4a3381e1ead933bbeeb76719f07f651f633f
Jschottert <83705512+Jschottert@users.noreply.github.com>
2021-06-15 12:25:42 +0200

    Add files via upload

---
+++ javascript/mijn eerste javascript/index.js
@@ -0,0 +1,29 @@
+let name = "jessica";
+console.log("jessica")
+
+// if is een reserved name dus kan niet worden gebruikt voor een variable//
+
+// getallen optellen uitkomst is 8
+let somNumber = 4 + 4;
+console.log(somNumber);
+// string optellen uitkomst is 44
+let somString = '4' + '4';
+console.log(somString);
+
+let somA = 1 + 1;
+let somB = 1 - 1;
+let somC = 5 * 1;
+let somD = 1 / 1;
+
+console.log(somA);
+console.log(somB);
+console.log(somC);
+console.log(somD);
+
+let leeftijd = 32;
+
+console.log(typeof (leeftijd));
+
+let leeftijdS = 'tweeëndertig';
+
+console.log(typeof (leeftijdS));
