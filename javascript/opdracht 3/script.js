4fed4fd5c8d945781859c955d08a9d5084b0488c
Jschottert <83705512+Jschottert@users.noreply.github.com>
2021-06-15 14:07:07 +0200

    Add files via upload

---
+++ javascript/opdracht 3/script.js
@@ -0,0 +1,29 @@
+// Age check 
+
+const age = 36;
+
+if (age >= 18) {
+    console.log('Welcome');
+} else {
+    console.log('Sorry je bent nog geen 18 jaar dus je mag nog niet naar binnen.');
+}
+
+// Ladiesnight
+
+const isFemale = true;
+
+if (isFemale) {
+    console.log('Je bent een vrouw, dus kaartjes mogen verkocht worden');
+} else {
+    console.log('Sorry ladiesnight is alleen voor de dames');
+}
+
+// Bob
+
+const driverStatus = 'bob';
+
+if (driverStatus == 'bob') {
+    console.log('Jij bent vanavond de bob!');
+} else {
+    console.log('Sorry om thuis te komen heb je een bob nodig!');
+}
